a6796201654b390ae89eeecb0d3cb2c566a9ac0d
Vicente Garofalo <vsgarofalojerez@gmail.com>
2026-07-02 13:02:35 +0200

    feat: Add ZenStack and env config dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import "./env";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+});
